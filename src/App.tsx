5330496a5a35475ee9f374bc75393245082348c6
Luis Henrique Gomes Cardoso <luis.cardoso@dotzmkt.com.br>
2023-09-17 14:32:02 -0300

    feat: Adding prompt areas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Button } from "./components/ui/button";
 import { Github } from "lucide-react"
 import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
   return (
-  <div>
+  <div className="min-h-screen flex flex-col">
     <div className="px-6 py-3 flex items-center justify-between border-b">
       <h1 className="text-xl font-bold">upload.ai</h1>
 
@@ -21,6 +22,21 @@ export function App() {
         </Button>
       </div>
     </div>
+
+    <main className="flex-1 p-6 flex gap-6">
+      <div className="flex flex-col flex-1 gap-4">
+        <div className="grid grid-rows-2 gap-4 flex-1">
+          <Textarea className="resize-none p-5 leading-relaxed" placeholder="Inclua o prompt para a IA..."/>
+          <Textarea className="resize-none p-5 leading-relaxed" placeholder="Resultado gerado pela IA" readOnly/>
+        </div>
+        
+        <p>Lembre-se: Você pode utilizar a variável <code className="text-orange-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do video selecionado</p>
+      </div>
+
+      <aside className="w-80 space-y-6">
+        <form className="space-y-6"></form>
+      </aside>
+    </main>
   </div>
   )
 }

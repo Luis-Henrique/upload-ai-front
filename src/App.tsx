19faf7c22470d1b75ac2f41b8b3a68e2704cb742
Luis Henrique Gomes Cardoso <luis.cardoso@dotzmkt.com.br>
2023-09-17 12:48:21 -0300

    feat: Adding shadcn components and creating header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { Button } from "./components/ui/button";
+import { Github } from "lucide-react"
+import { Separator } from "./components/ui/separator";
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+  <div>
+    <div className="px-6 py-3 flex items-center justify-between border-b">
+      <h1 className="text-xl font-bold">upload.ai</h1>
+
+      <div className="flex items-center gap-3">
+        <span className="text-small text-muted-foreground">
+          Desenvolvido com 🧡 no NLW da Rocketseat
+        </span>
+
+        <Separator orientation="vertical" className='h-6'/>
+
+        <Button variant="secondary">
+          <Github className="w-4 h-4 mr-2" />
+          Github
+        </Button>
+      </div>
+    </div>
+  </div>
   )
 }

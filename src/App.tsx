a714fc8ceee1f3761423bc017e926c988c07560c
Luis Henrique Gomes Cardoso <luis.cardoso@dotzmkt.com.br>
2023-09-18 10:43:07 -0300

    feat(mainpage): finishing mainpage contant and adding labels

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Button } from "./components/ui/button";
-import { Github } from "lucide-react"
+import { Github, FileVideo, Upload, Wand2 } from "lucide-react"
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import { Slider } from "./components/ui/slider";
 
 export function App() {
   return (
@@ -34,7 +36,88 @@ export function App() {
       </div>
 
       <aside className="w-80 space-y-6">
-        <form className="space-y-6"></form>
+        <form className="space-y-6">
+          <label htmlFor="video" className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5">
+            <FileVideo className="w-4 h-4"/>
+            Selecione um video
+          </label>
+
+          <input type="file" id="video" accept="video/mp4" className="sr-only"/>
+
+          <Separator />
+
+          <div className="space-y-3">
+            <label htmlFor="transcription_prompt">Prompt de transcrição</label>
+            <Textarea 
+              id="transcription_prompt"
+              className="h-20 leading-relaxed resize-none"
+              placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
+            />
+
+            <Button type="submit" className="w-full">
+              Carregar vídeo
+
+              <Upload className="w-4 h-4 ml-2" />
+            </Button>
+          </div>
+        </form>
+
+        <Separator />
+
+        <form className="space-y-6">
+          <div className="space-y-4">
+            <label>Prompt</label>
+
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder="Selecione um prompt..."/>
+                <SelectContent>
+                  <SelectItem value="title">Titulo do youtube</SelectItem>
+                  <SelectItem value="description">Descrição do youtube</SelectItem>
+                </SelectContent>
+              </SelectTrigger>
+            </Select>
+          </div>
+
+          <Separator />
+
+          <div className="space-y-4">
+            <label>Modelo</label>
+
+            <Select disabled defaultValue="gpt3.5">
+              <SelectTrigger>
+                <SelectValue />
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </SelectTrigger>
+              <span className="block text-xs text-muted-foreground italic">
+                Você poderá customizar essa opção em breve
+              </span>
+            </Select>
+          </div>
+
+          <Separator />
+
+          <div className="space-y-4">
+            <label>Temperatura</label>
+            <Slider 
+              min={0}
+              max={1}
+              step={0.1}
+            />
+            <span className="block text-xs text-muted-foreground italic leading-relaxed">
+              Valores mais altos tendem a deixar o resultado mais criativo porém com possíveis erros
+            </span>
+          </div>
+
+          <Separator />
+
+          <Button type="submit" className="w-full">
+            Executar
+            <Wand2 className="h-4 ml-2" />
+          </Button>
+        </form>
       </aside>
     </main>
   </div>
